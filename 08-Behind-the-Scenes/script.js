28e018d028c428b09608d1c2329123324fe0835c
David Garbanzo <davidgq10@hotmail.com>
2022-08-19 17:00:46 -0600

    practicing variables scope

---
+++ 08-Behind-the-Scenes/script.js
@@ -0,0 +1,25 @@
+// 'use strict';
+
+function calcAge(birthYear) {
+  const firstName = 'Fernando';
+  const age = 2037 - birthYear;
+  function printAge() {
+    const output = `${firstName}, you are ${age}, born in ${birthYear}`;
+    console.log(output);
+    if (1981 <= birthYear <= 1996) {
+      var millenial = true;
+      const str = `Oh!, and you're a millenial ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+    }
+
+    add(2, 3);
+  }
+  printAge();
+}
+
+const firstName = 'David';
+calcAge(1988);
